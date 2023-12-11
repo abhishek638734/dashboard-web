ab8cdb138a94abe427a304b22f2f85163079e415
abhishek638734 <ay9454002598@gmail.com>
2023-12-12 00:43:45 +0530

    23.12.12.01

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -15,6 +15,7 @@ export const ContextProvider = ({ children }) => {
   const [themeSettings, setThemeSettings] = useState(false);
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
+  const [submitButtonClicked, setSubmitButtonClicked] = useState(false);
 
   const setMode = (e) => {
     setCurrentMode(e.target.value);
@@ -30,7 +31,8 @@ export const ContextProvider = ({ children }) => {
 
   return (
     // eslint-disable-next-line react/jsx-no-constructed-context-values
-    <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
+    <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings,submitButtonClicked,
+    setSubmitButtonClicked }}>
       {children}
     </StateContext.Provider>
   );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import bg from '../data/bg.jpg';
 
+import { useStateContext } from '../contexts/ContextProvider';
+
 const DropDown = ({ currentMode }) => (
     <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
       <DropDownListComponent id="time" fields={{ text: 'Time', value: 'Id' }} style={{ border: 'none', color: (currentMode === 'Dark') && 'white' }} value="1" dataSource={dropdownData} popupHeight="220px" popupWidth="120px" />
@@ -10,6 +12,12 @@ const DropDown = ({ currentMode }) => (
 
 const Landing = () => {
 
+  const { setSubmitButtonClicked } = useStateContext();
+
+  const handleButtonClick = () => {
+    // Perform any additional logic you need
+    setSubmitButtonClicked(true);
+  };
   return (
 
     
@@ -17,13 +25,16 @@ const Landing = () => {
     <div className="relative p-0">
     <div className="absolute inset-0 bg-cover bg-center h-screen" style={{ backgroundImage: `url(${bg})` }}/>
     <div className="relative">
-    <form action="" className='flex items-center flex-col relative top-40'>
+
+      {/* please change below div to the form when writing login logic in backend */}
+    <div className='flex items-center flex-col relative top-40'>
       <div>
       <label htmlFor="" className='text-white font-medium text-base'>Email : </label>
       <input type="email" className='border-none outline-none px-2 py-1 box-border shadow-md rounded-md bg-white' />
       </div>
-      <button className='my-5 bg-blue-500 focus:outline-none rounded-md px-4 py-1 cursor-pointer text-white'>Submit</button>
-    </form>
+      <button onClick={handleButtonClick} className='my-5 bg-blue-500 focus:outline-none rounded-md px-4 py-1 cursor-pointer text-white'>Submit</button>  
+    </div>
+
     </div>
     </div>
   )
